2d4272973f11f662413a4842a78b4bc588a598e2
Kartikjain2626 <kartikjain2626@gmail.com>
2024-09-08 18:02:23 +0530

    Day 6

---
+++ Array/1464_Maximum_Product_of_Two_Elements_in_an_Array.java
@@ -0,0 +1,25 @@
+//Porblem Statement no : 1464
+//Porblem Statement Topic : Maximum Product of Two Elements in an Array
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int smax = -1;
+        int max = -1;
+
+        for(int i=0;i<nums.length;i++){
+            
+            if(max < nums[i])
+            {
+                smax = max;
+                max = nums[i];
+            }
+            else if(smax<nums[i])
+            {
+                smax = nums[i];
+            }
+        }
+
+        int ans = (max-1) * (smax-1);
+        return ans;
+    }
+}
