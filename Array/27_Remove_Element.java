3e02c79d5c727d930787a99631ab2ca20713eab6
Kartikjain2626 <kartikjain2626@gmail.com>
2024-09-10 19:49:51 +0530

    Day 8

---
+++ Array/27_Remove_Element.java
@@ -0,0 +1,16 @@
+//Porblem Statement no : 27  
+//Porblem Statement Topic : . Remove Element
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int ptr = 0;
+
+        for (int i=0; i<nums.length; i++){
+            if(nums[i]!=val){
+                nums[ptr] = nums[i];
+                ptr++;
+            }
+        }
+        return ptr;
+    }
+}
