8d1900d91cf74d2a18850511dadb83be27779535
Kartikjain2626 <kartikjain2626@gmail.com>
2024-10-03 10:34:18 +0530

    Day 29

---
+++ Array/53_Maximum_Subarray.java
@@ -0,0 +1,22 @@
+//Porblem Statement no : 53
+//Porblem Statement Topic : Maximum Subarray
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currsum = nums[0];
+        int max = nums[0];
+
+        for(int i=1; i<nums.length; i++){
+            if(currsum + nums[i]>nums[i]){
+                currsum += nums[i];
+            }
+            else{
+                currsum = nums[i];
+            }
+
+            max = Math.max(max, currsum);
+        }
+        return max;
+    }
+}
