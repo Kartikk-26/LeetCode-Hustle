ac9a4551f6931da0f6e6a578c449a4dd01f0cce5
Kartikjain2626 <kartikjain2626@gmail.com>
2024-11-14 17:11:52 +0530

    14 Nov

---
+++ POTD/2064. Minimized Maximum of Products Distributed to Any Store.java
@@ -0,0 +1,34 @@
+//Date : 14.11.2024
+//Porblem Statement no : 2064.  
+//Porblem Statement Topic : Minimized Maximum of Products Distributed to Any Store
+
+
+class Solution {
+    private boolean solve(int n, int[] quantities, int item) {
+        if (item == 0) return false;
+        int store = 0;
+        for (int product : quantities) {
+            store += (product - 1) / item + 1;
+            if (store > n) return false;
+        }
+        return true;
+    }
+
+    public int minimizedMaximum(int n, int[] quantities) {
+        int low = 1;
+        int high = Arrays.stream(quantities).max().getAsInt();
+        int ans = -1;
+        
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (solve(n, quantities, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+}
