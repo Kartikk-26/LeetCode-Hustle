7314f668e87def9287d783795a78aea1064ab5b3
Kartikjain2626 <kartikjain2626@gmail.com>
2024-11-20 22:00:18 +0530

    20 Nov

---
+++ POTD/2516. Take K of Each Character From Left and Right.java
@@ -0,0 +1,32 @@
+//Date : 20.11.2024
+//Porblem Statement no : 2516.     
+//Porblem Statement Topic : Take K of Each Character From Left and Right
+
+class Solution {
+    boolean isValid(int[] freq, int k){
+         return freq[0] >= k && freq[1] >= k && freq[2] >= k;
+    }
+    public int takeCharacters(String s, int k) {
+        int[] freq = new int[3];
+
+        for(char c: s.toCharArray()){
+            freq[c-'a']+=1;
+        }
+
+        if(!isValid(freq,k))
+            return -1;
+
+        int i=0, j=0, max = -1;
+        while(j<s.length()){
+            freq[s.charAt(j)-'a']-=1;
+             while(!isValid(freq,k)){
+                freq[s.charAt(i)-'a']+=1;
+                i++;
+             }
+             max = Math.max(max,j-i+1);
+             j++;
+        }
+
+        return s.length()-max;
+    }
+}
