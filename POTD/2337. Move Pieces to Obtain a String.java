d65960c14b84527154611c6a8254922a2e0a963d
Kartikjain2626 <kartikjain2626@gmail.com>
2024-12-05 11:16:48 +0530

    05 Dec

---
+++ POTD/2337. Move Pieces to Obtain a String.java
@@ -0,0 +1,51 @@
+//Date : 05.12.2024
+//Porblem Statement no : 2337.          
+//Porblem Statement Topic : Move Pieces to Obtain a String
+
+
+
+class Solution {
+    public boolean canChange(String start, String target) {
+        //If both string are equal
+        if(start.equals(target)){
+            return true;
+        }
+
+        int waitL = 0;
+        int waitR = 0;
+
+        for(int i=0; i<start.length(); i++){
+            char curr = start.charAt(i);
+            char goal = target.charAt(i);
+
+            if(curr =='R'){
+                if(waitL > 0){
+                    return false;
+                }
+                waitR++;
+            }
+
+            if(goal == 'L'){
+                if(waitR > 0){
+                    return false;
+                }
+                waitL++;
+            }
+
+            if(goal == 'R'){
+                if(waitR == 0){
+                    return false;
+                }
+                waitR--;
+            }
+
+            if(curr == 'L'){
+                if(waitL== 0){
+                    return false;
+                }
+                waitL--;
+            }
+        }
+        return waitL==0 && waitR==0;
+    }
+}
