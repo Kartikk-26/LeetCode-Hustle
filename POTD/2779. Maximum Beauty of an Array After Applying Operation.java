b66f11236a955f10f3c302e2d20d1394197326a1
Kartikjain2626 <kartikjain2626@gmail.com>
2024-12-11 19:39:40 +0530

    11 Dec

---
+++ POTD/2779. Maximum Beauty of an Array After Applying Operation.java
@@ -0,0 +1,32 @@
+//Date : 11.12.2024
+//Porblem Statement no : 2779.              
+//Porblem Statement Topic :  Maximum Beauty of an Array After Applying Operation
+
+class Solution {
+    public int maximumBeauty(int[] nums, int k) {
+        if (nums.length == 1)
+            return 1;
+
+        int maxBeauty = 0;
+        int maxValue = 0;
+
+        for (int num : nums)
+            maxValue = Math.max(maxValue, num);
+
+        int[] count = new int[maxValue + 1];
+
+        for (int num : nums) {
+            count[Math.max(num - k, 0)]++;
+            count[Math.min(num + k + 1, maxValue)]--;
+        }
+
+        int currentSum = 0;
+
+        for (int val : count) {
+            currentSum += val;
+            maxBeauty = Math.max(maxBeauty, currentSum);
+        }
+
+        return maxBeauty;
+    }
+}
