2a4600a8b21d897930182a1d34740da905546f70
Kartikjain2626 <kartikjain2626@gmail.com>
2025-01-01 22:45:57 +0530

    01 Jan

---
+++ POTD/1422. Maximum Score After Splitting a String.java
@@ -0,0 +1,30 @@
+//Date : 01.01.2025
+//Porblem Statement no : 1422.                               
+//Porblem Statement Topic : Maximum Score After Splitting a String
+
+public class Solution {
+    public int maxScore(String inputStr) {
+        int totalOnes = 0;
+        int zerosCount = 0;
+        int onesCount = 0;
+        int bestScore = Integer.MIN_VALUE;
+
+        // Count total ones in the string
+        for (char ch : inputStr.toCharArray()) {
+            if (ch == '1') totalOnes++;
+        }
+
+        // Traverse the string and calculate scores
+        for (int i = 0; i < inputStr.length() - 1; i++) {
+            if (inputStr.charAt(i) == '0') zerosCount++;
+            else onesCount++;
+
+            // Calculate score
+            int currentScore = zerosCount + (totalOnes - onesCount);
+            bestScore = Math.max(bestScore, currentScore);
+
+        }
+
+        return bestScore;
+    }
+}
