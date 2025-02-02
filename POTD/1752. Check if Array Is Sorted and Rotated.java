5ac486a263ca8addba602173bfebd4f1a932916e
Kartikjain2626 <kartikjain2626@gmail.com>
2025-02-02 11:52:10 +0530

    02 Feb

---
+++ POTD/1752. Check if Array Is Sorted and Rotated.java
@@ -0,0 +1,16 @@
+//Date : 02.02.2025
+//Porblem Statement no : 1752.                              
+//Porblem Statement Topic : Check if Array Is Sorted and Rotated
+
+class Solution {
+    public boolean check(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            if(nums[i]> nums[(i+1)%n]  && ++count >1){
+                return false;
+            }
+        }
+        return count<=1;
+    }
+}
