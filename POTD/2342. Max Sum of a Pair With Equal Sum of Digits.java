dd2b7c893eae1d310abd121324fe466326c43cc4
Kartikjain2626 <kartikjain2626@gmail.com>
2025-02-12 17:27:09 +0530

    12 Feb

---
+++ POTD/2342. Max Sum of a Pair With Equal Sum of Digits.java
@@ -0,0 +1,24 @@
+//Date : 12.02.2025                        
+//Porblem Statement Number & Topic : 2342. Max Sum of a Pair With Equal Sum of Digits
+
+class Solution {
+    public int maximumSum(int[] nums) {
+        int[] mp = new int[82];
+        Arrays.fill(mp, -1);
+        int ans = -1;
+
+        for (int num : nums) {
+            int sumDigits = 0, temp = num;
+            while (temp > 0) {
+                sumDigits += temp % 10;
+                temp /= 10;
+            }
+
+            if (mp[sumDigits] != -1)
+                ans = Math.max(ans, num + mp[sumDigits]);
+
+            mp[sumDigits] = Math.max(mp[sumDigits], num);
+        }
+        return ans;
+    }
+}
