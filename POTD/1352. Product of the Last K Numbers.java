6523c768e5545efa0e6f5dfeb06d33ddcbe1ec8b
Kartikjain2626 <kartikjain2626@gmail.com>
2025-02-14 17:15:20 +0530

    14 Feb

---
+++ POTD/1352. Product of the Last K Numbers.java
@@ -0,0 +1,23 @@
+//Date : 14.02.2025                        
+//Porblem Statement Number & Topic : 1352. Product of the Last K Numbers
+
+class ProductOfNumbers {
+    List<Integer> prefixProduct;
+    public ProductOfNumbers() {
+        prefixProduct = new ArrayList<>();
+        prefixProduct.add(1);
+    }
+    public void add(int num) {
+        if (num == 0) {
+            prefixProduct.clear();
+            prefixProduct.add(1);
+        } else {
+            prefixProduct.add(num * prefixProduct.get(prefixProduct.size() - 1));
+        }
+    }
+    public int getProduct(int k) {
+        int size = prefixProduct.size();
+        if (size <= k) return 0;
+        return prefixProduct.get(size - 1) / prefixProduct.get(size - k - 1);
+    }
+}
