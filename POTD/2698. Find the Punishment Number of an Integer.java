70755b6f800c801e886e3f3684150cec55af630b
Kartikjain2626 <kartikjain2626@gmail.com>
2025-02-15 21:34:01 +0530

    15 Feb

---
+++ POTD/2698. Find the Punishment Number of an Integer.java
@@ -0,0 +1,26 @@
+//Date : 15.02.2025                        
+//Porblem Statement Number & Topic : 2698. Find the Punishment Number of an Integer
+
+class Solution {
+    public int punishmentNumber(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (canPartition(String.valueOf(i * i), 0, i)) {
+                sum += (i * i);
+            }
+        }
+        return sum;
+    }
+
+    private boolean canPartition(String num, int index, int target) {
+        if (index == num.length()) return target == 0;
+
+        int sum = 0;
+        for (int i = index; i < num.length(); i++) {
+            sum = sum * 10 + (num.charAt(i) - '0');
+            if (sum > target) break;
+            if (canPartition(num, i + 1, target - sum)) return true;
+        }
+        return false;
+    }
+}
