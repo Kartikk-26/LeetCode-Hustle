1e0214a3f56d7841420c7aed09cabb71315996e3
Kartikjain2626 <kartikjain2626@gmail.com>
2025-02-18 22:18:01 +0530

    18 Feb

---
+++ POTD/2375. Construct Smallest Number From DI String.java
@@ -0,0 +1,24 @@
+//Date : 18.02.2025                        
+//Porblem Statement Number & Topic : 2375. Construct Smallest Number From DI String
+
+class Solution {
+    public String smallestNumber(String pattern) {
+        StringBuilder ans = new StringBuilder("1");
+        StringBuilder temp = new StringBuilder();
+        
+        for (int i = 0; i < pattern.length(); i++) {
+            if (pattern.charAt(i) == 'I') {
+                // Flush temp and add next
+                ans.append(temp).append((char)('2' + i));
+                temp.setLength(0);
+            } else {
+                // Store in temp
+                temp.insert(0, ans.charAt(ans.length() - 1));
+                ans.setLength(ans.length() - 1);
+                ans.append((char)('2' + i));
+            }
+        }
+        
+        return ans.append(temp).toString();
+    }
+}
